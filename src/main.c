2dd35e63340dbb25fac910c1bbf4d93ede749779
unknown <aa@a>
2024-03-05 15:54:14 +0800

    prism

---
+++ src/main.c
@@ -0,0 +1,111 @@
+
+#include "freeglut_std.h"
+#include <GL/gl.h>
+#include <stdio.h>
+/*** freeglut***/
+#include <freeglut.h>
+
+void ChangeSize(int, int);
+void RenderScene(void);
+void MenuCallback(int);
+
+GLenum glShadeType = GL_SMOOTH;
+float angle = 45;
+
+void Loop() {
+  angle += 0.1f;
+  glutPostRedisplay();
+}
+
+int main(int argc, char **argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize(800, 800);
+  glutInitWindowPosition(600, 80);
+  glutCreateWindow("Simple Triangle");
+
+  glutCreateMenu(MenuCallback);
+  glutAddMenuEntry("GL_SMOOTH", 1);
+  glutAddMenuEntry("GL_FLAT", 2);
+  glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+  glutReshapeFunc(ChangeSize);
+  glutDisplayFunc(RenderScene);
+  glutIdleFunc(Loop);
+  glutMainLoop(); // http://www.programmer-club.com.tw/ShowSameTitleN/opengl/2288.html
+  return 0;
+}
+void ChangeSize(int w, int h) {
+  // printf("Window Size= %d X %d\n", w, h);
+  glViewport(0, 0, w, h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(-10, 10, -10, 10,
+          -10, 100);
+  glMatrixMode(GL_MODELVIEW);  
+  glLoadIdentity();
+}
+void RenderScene(void) {
+
+  glClearColor(0, 0, 0, 1.0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glMatrixMode(GL_MODELVIEW);
+  glEnable(GL_DEPTH_TEST);
+
+  glLoadIdentity();
+  gluLookAt(
+    0, 0, 10,
+    0, 0, 0,
+    0, 1, 0);
+  glShadeModel(glShadeType);
+
+  glRotatef(angle,1,1,0);
+  glBegin(GL_TRIANGLES);
+    glColor3f(1,1,0);
+    glVertex3f(-5, 0, 3);
+    glVertex3f(0, 7, 3);
+    glVertex3f(5, 0, 3);
+  glEnd();
+  glBegin(GL_TRIANGLES);
+    glColor3f(1,0,1);
+    glVertex3f(-5, 0, -3);
+    glVertex3f(0, 7, -3);
+    glVertex3f(5, 0, -3);
+  glEnd();
+  glBegin(GL_TRIANGLE_FAN);
+    glColor3f(1,0,0);
+    glVertex3f(-5, 0, 3);
+    glVertex3f(-5, 0, -3);
+    glVertex3f(0, 7, -3);
+    glVertex3f(0, 7, 3);
+  glEnd();
+  glBegin(GL_TRIANGLE_FAN);
+    glColor3f(0,0,1);
+    glVertex3f(5, 0, 3);
+    glVertex3f(5, 0, -3);
+    glVertex3f(0, 7, -3);
+    glVertex3f(0, 7, 3);
+  glEnd();
+  glBegin(GL_TRIANGLE_FAN);
+    glColor3f(0,1,1);
+    glVertex3f(5, 0, 3);
+    glVertex3f(-5, 0, 3);
+    glVertex3f(-5, 0, -3);
+    glVertex3f(5, 0, -3);
+  glEnd();
+
+  glutSwapBuffers();
+}
+
+void MenuCallback(int value) {
+  switch (value) {
+  case 1:
+    glShadeType = GL_SMOOTH;
+    break;
+  case 2:
+    glShadeType = GL_FLAT;
+    break;
+  }
+  glutPostRedisplay();
+}
