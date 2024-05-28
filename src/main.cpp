49976952abb18fde1a5b8ef80c6bbaba95d005bd
x06lan <x065p4mp6@gmail.com>
2024-05-28 15:04:58 +0800

    lab11

--- src/main.cpp
+++ src/main.cpp
@@ -16,457 +16,272 @@
 #include <vector>
 #include <random>
 
-void ChangeSize(int, int);
-void RenderScene(void);
-void MenuCallback1(int);
-void MenuCallback2(int);
-void MenuCallback3(int);
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
 
-GLenum shadeType = GL_SMOOTH;
-GLenum renderMode = GL_TRIANGLES;
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
-std::vector<Vector3d> vertices;
-std::vector<FaceIndices> faces;
-BoundBox box;
+// #include <opencv2/core/core.hpp>
+// #include <opencv2/highgui/highgui.hpp>
 
-// std::vector<Vector2d> points = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-// std::vector<Vector2d> points = {{0, 0}, {1, 0}, {1, 1}, {0.0, 1}, {-1, 1}, {-1, 0}, {1, -1}, {0.0, -1}, {-1, -1}};
-std::vector<Vector2d> points = {{0, -1}, {0.1, 0.51}, {-0.8, 0.5}, {-0.8, 0.5}, {-0.8, 0.5}};
-std::vector<Vector3d> colors = {{0, 1, 1}, {0.5, 0.5, 0.0}, {0.5, 0.5, 1}, {0.7, 0.1, 0}, {0.5, 0.5, 0.5}};
-// int windowx = 1920 / 2;
-// int windowy = 1080 - 40;
-int windowx = 1080 - 40;
-int windowy = 1080 - 40;
-float lines = 100;
-float padding = 0.05;
+// Lighting data
+GLfloat lightAmbient[] = {0.2f, 0.2f, 0.2f, 1.0f};
+GLfloat lightDiffuse[] = {0.7f, 0.7f, 0.7f, 1.0f};
+GLfloat lightSpecular[] = {0.9f, 0.9f, 0.9f};
+GLfloat materialColor[] = {0.8f, 0.0f, 0.0f};
+GLfloat vLightPos[] = {-80.0f, 120.0f, 100.0f, 0.0f};
+GLfloat ground[3][3] = {{0.0f, -25.0f, 0.0f},
+                        {10.0f, -25.0f, 0.0f},
+                        {10.0f, -25.0f, -10.0f}};
 
-Vector3d camera = {0, 0, -1};
-Vector3d target = {0, 0, 0};
+GLuint textures[4];
 
-bool update = false;
-unsigned int update_time = 1;
+int nStep = 0;
 
-int draw_id = 0;
-void Timer(int value)
-{
-  // RenderScene();
-  glutPostRedisplay(); // Post re-paint request to activate display()
-
-  draw_id += 1;
-  glutTimerFunc(update_time, Timer, 0); // next Timer call milliseconds later
-}
-
-void Loop()
-{
-  RenderScene();
-  // glutPostRedisplay();
-  // angle.y += ydelta;
-  // xangle += xdelta;
-  glutPostRedisplay();
-}
-
-void SpecialKeyHandler(int key, int x, int y)
+void MyKeyboard(unsigned char key, int x, int y)
 {
   switch (key)
   {
-  case GLUT_KEY_RIGHT:
-    // angle.y += 10.0f;
+  case 'r':
+    if (nStep < 4)
+      nStep++;
+    else
+      nStep = 0;
     break;
-  case GLUT_KEY_LEFT:
-    // angle.y -= 10.0f;
-    break;
-  case GLUT_KEY_UP:
-    // angle.x += 10.0f;
-    break;
-  case GLUT_KEY_DOWN:
-    // angle.x -= 10.0f;
+  default:
     break;
   }
-}
-void MouseHandler(int button, int state, int x, int y)
-{
 
-  if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-  {
-    float mousex = ((float)x / (float)windowx) * 2 - 1;
-    float mousey = -2 * ((float)y / (float)windowy) + 1;
-    mousex = clamp(mousex * (1.0 + padding), -1, 1);
-    mousey = clamp(mousey * (1.0 + padding), -1, 1);
-
-    // points.insert(points.begin(), {mousex, mousey});
-    // points.pop_back();
-    points.push_back({mousex, mousey});
-    colors.push_back({generateRandomFloat(x), generateRandomFloat(y), generateRandomFloat(x + y)});
-  }
-  if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
-  {
-    // mousex = ((float)x / (float)windowx) * 2 - 1;
-    // mousey = -2 * ((float)y / (float)windowy) + 1;
-  }
-  // printf("mouse x=%.2f,y=%.2f\n", mousex, mousey);
-  // printf("w=%d,h=%d\n", windowx, windowy);
-}
-
-void MenuCallback1(int value)
-{
-  switch (value)
-  {
-  case 10:
-    lines = 10;
-    break;
-  case 15:
-    lines = 15;
-    break;
-  case 20:
-    lines = 20;
-    break;
-  case 100:
-    lines = 100;
-    break;
-  }
   glutPostRedisplay();
 }
 
-void ScaleMatrix(GLfloat x, GLfloat y, GLfloat z)
+// Called to draw scene
+void RenderScene(void)
 {
-  GLfloat m[16] = {
-      x, 0, 0, 0,
-      0, y, 0, 0,
-      0, 0, z, 0,
-      0, 0, 0, 1};
-  glMultMatrixf(m);
-}
+  // Clear the window with current clearing color
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glShadeModel(GL_SMOOTH);
+  glEnable(GL_NORMALIZE);
 
-void NormalKeyHandler(unsigned char key, int x, int y)
-{
-  switch (key)
+  glPushMatrix();
+
+  // Draw plane that the cube rests on
+  glDisable(GL_LIGHTING);
+  glColor3ub(255, 255, 255);
+  if (nStep >= 1)
   {
-  case 'q':
-    // trans.x += 0.01f * box.get_radius();
-    break;
+    glEnable(GL_TEXTURE_2D); // 啟動openGL的2D材質填充模式
 
-  case '1':
-    // reset
-    break;
-  }
-}
+    // 將textures[0]中所儲存的材質貼在四邊形上
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-100.0f, -25.3f, -100.0f);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-100.0f, -25.3f, 100.0f);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(100.0f, -25.3f, 100.0f);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(100.0f, -25.3f, -100.0f);
+    glEnd();
 
-float remap(float value, float padding, float lines)
-{
-  float v = floor((value + 1) * 0.5 * lines);
-  v = clamp(v, 0, lines - 1);
-  float unlines = 1.0 / lines;
-  v = v * unlines * 2.0 - 1 + unlines;
-  return v;
-}
-float blockId(float value, float padding, float lines)
-{
-  float v = floor((value + 1) * 0.5 * lines);
-  v = clamp(v, 0, lines - 1);
-  return v;
-}
-float blockIdCoord(float v)
-{
-  // v = clamp(v, 0, lines - 1);
-  float unlines = 1.0 / lines;
-  v = v * unlines * 2.0 - 1 + unlines;
-  return v;
-}
-float lerp(float a, float b, float t)
-{
-  return a + (b - a) * t;
-}
-Vector2d lerp(Vector2d a, Vector2d b, float t)
-{
-  Vector2d v;
-  v.x = lerp(a.x, b.x, t);
-  v.y = lerp(a.y, b.y, t);
-  return v;
-}
-Vector3d lerp(Vector3d a, Vector3d b, float t)
-{
-  Vector3d v;
-  v.x = lerp(a.x, b.x, t);
-  v.y = lerp(a.y, b.y, t);
-  v.z = lerp(a.z, b.z, t);
-  return v;
-}
-
-void drawLine(Vector2d a, Vector2d b, Vector3d color1, Vector3d color2)
-{
-  float x1 = blockId(a.x, padding, lines);
-  float y1 = blockId(a.y, padding, lines);
-  float x2 = blockId(b.x, padding, lines);
-  float y2 = blockId(b.y, padding, lines);
-
-  int magic_number_x = x2 - x1 > 0 ? 1 : -1;
-  int magic_number_y = y2 - y1 > 0 ? 1 : -1;
-
-  int dx = (x2 - x1) * magic_number_x;
-  int dy = (y2 - y1) * magic_number_y;
-
-  // start point / init point
-  // drawPoint(X1, Y1);
-  // printf("dx=%d,dy=%d\n", dx, dy);
-
-  if (dy <= dx)
-  {
-    int d = dy - (dx / 2);
-    int y = 0;
-    for (int x = 1; x < dx; x++)
-    {
-      // printf("x=%d,y=%d,d=%d\n", x, y, d);
-      if (d < 0)
-      {
-        // E or East is chosen
-        d = d + dy;
-      }
-      else
-      {
-        // NE or North East is chosen
-        d += (dy - dx);
-        y++;
-      }
-
-      auto color = lerp(color1, color2, (float)x / dx);
-      glColor3f(color.x, color.y, color.z);
-      float px = blockIdCoord(x1 + x * magic_number_x);
-      float py = blockIdCoord(y1 + y * magic_number_y);
-      glBegin(GL_POINTS);
-      glVertex3f(px, py, 5);
-      glEnd();
-    }
+    glDisable(GL_TEXTURE_2D);
   }
   else
   {
-    int d = dx - (dy / 2);
-    int x = 0;
-    for (int y = 1; y < dy; y++)
-    {
-      // printf("x=%d,y=%d,d=%d\n", x, y, d);
-      if (d < 0)
-      {
-        // E or East is chosen
-        d = d + dx;
-      }
-      else
-      {
-        // NE or North East is chosen
-        d += (dx - dy);
-        x++;
-      }
-      auto color = lerp(color1, color2, (float)y / dy);
-      glColor3f(color.x, color.y, color.z);
-      float px = blockIdCoord(x1 + x * magic_number_x);
-      float py = blockIdCoord(y1 + y * magic_number_y);
-      glBegin(GL_POINTS);
-      glVertex3f(px, py, 5);
-      glEnd();
-    }
+    glColor3f(0.0f, 0.0f, 0.90f); // Blue
+    glBegin(GL_QUADS);
+    glVertex3f(-100.0f, -25.3f, -100.0f);
+    glVertex3f(-100.0f, -25.3f, 100.0f);
+    glVertex3f(100.0f, -25.3f, 100.0f);
+    glVertex3f(100.0f, -25.3f, -100.0f);
+    glEnd();
   }
+
+  // Set drawing color to Red
+  glColor3f(1.0f, 0.0f, 0.0f);
+
+  // Move the cube slightly forward and to the left
+  glTranslatef(-10.0f, 0.0f, 10.0f);
+
+  glColor3ub(255, 255, 255);
+
+  if (nStep >= 2)
+  {
+    glEnable(GL_TEXTURE_2D);
+  }
+  // Front Face (before rotation)
+  glBindTexture(GL_TEXTURE_2D, textures[1]);
+  glBegin(GL_QUADS);
+  glTexCoord2f(1.0f, 1.0f);
+  glVertex3f(25.0f, 25.0f, 25.0f);
+  glTexCoord2f(1.0f, 0.0f);
+  glVertex3f(25.0f, -25.0f, 25.0f);
+  glTexCoord2f(0.0f, 0.0f);
+  glVertex3f(-25.0f, -25.0f, 25.0f);
+  glTexCoord2f(0.0f, 1.0f);
+  glVertex3f(-25.0f, 25.0f, 25.0f);
+  glEnd();
+
+  glDisable(GL_TEXTURE_2D);
+
+  if (nStep >= 3)
+  {
+    glEnable(GL_TEXTURE_2D);
+  }
+  // Top of cube
+  glBindTexture(GL_TEXTURE_2D, textures[2]);
+  glBegin(GL_QUADS);
+  // Front Face
+  glTexCoord2f(0.0f, 0.0f);
+  glVertex3f(25.0f, 25.0f, 25.0f);
+  glTexCoord2f(1.0f, 0.0f);
+  glVertex3f(25.0f, 25.0f, -25.0f);
+  glTexCoord2f(1.0f, 1.0f);
+  glVertex3f(-25.0f, 25.0f, -25.0f);
+  glTexCoord2f(0.0f, 1.0f);
+  glVertex3f(-25.0f, 25.0f, 25.0f);
+  glEnd();
+
+  glDisable(GL_TEXTURE_2D);
+
+  if (nStep >= 4)
+  {
+    glEnable(GL_TEXTURE_2D);
+  }
+  // Last two segments for effect
+  glBindTexture(GL_TEXTURE_2D, textures[3]);
+  glBegin(GL_QUADS);
+  glTexCoord2f(1.0f, 1.0f);
+  glVertex3f(25.0f, 25.0f, -25.0f);
+  glTexCoord2f(1.0f, 0.0f);
+  glVertex3f(25.0f, -25.0f, -25.0f);
+  glTexCoord2f(0.0f, 0.0f);
+  glVertex3f(25.0f, -25.0f, 25.0f);
+  glTexCoord2f(0.0f, 1.0f);
+  glVertex3f(25.0f, 25.0f, 25.0f);
+  glEnd();
+
+  glDisable(GL_TEXTURE_2D);
+
+  glTranslatef(-10.0f, 0.0f, 10.0f);
+
+  glPopMatrix();
+
+  // Flush drawing commands
+  glutSwapBuffers();
 }
 
-bool isLeft(Vector2d l1, Vector2d l2, Vector2d p)
+// This function does any needed initialization on the rendering
+// context.
+void load_image(GLuint *textures, const char *path)
 {
-  return (l2.x - l1.x) * (p.y - l1.y) - (l2.y - l1.y) * (p.x - l1.x) >= 0.0;
-}
+  int width, height, channels;
+  unsigned char *imageData = stbi_load(path, &width, &height, &channels, 0);
 
-int main(int argc, char **argv)
+  glGenTextures(1, textures);
+  glBindTexture(GL_TEXTURE_2D, *textures);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  if (imageData)
+  {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+                 width, height,
+                 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
+    stbi_image_free(imageData);
+    printf("success");
+  }
+  else
+  {
+    printf("nmsl");
+  }
+
+  glEnable(GL_TEXTURE_2D);
+  glBindTexture(GL_TEXTURE_2D, *textures);
+}
+void SetupRC()
 {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-  glutInitWindowSize(windowx, windowy);
-  glutInitWindowPosition(600, 80);
-  glutCreateWindow("2d grid");
+  GLbyte *pBytes;
+  GLint nWidth, nHeight, nComponents;
+  GLenum format;
 
-  // printf("center=(%.2f,%.2f,%.2f)\n", box.center.x, box.center.y, box.center.z);
-  // printf("radius=%.2f\n", box.get_radius());
-  int sub1 = glutCreateMenu(MenuCallback1);
-  glutAddMenuEntry("10", 10);
-  glutAddMenuEntry("15", 15);
-  glutAddMenuEntry("20", 20);
-  glutAddMenuEntry("100", 100);
+  // glEnable(GL_TEXTURE_2D);
+  // glBindTexture(GL_TEXTURE_2D, texture);
+  // // Black background
+  // glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-  glutCreateMenu(nullptr);
-  glutAddSubMenu("grid", sub1);
+  // glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE); // 設定openGL材質紋理的參數和材質的組合模式
+  // glGenTextures(4, textures);                                  // 註冊一個大小為4的陣列讓openGL儲存材質，名稱為textures
 
-  glutAttachMenu(GLUT_RIGHT_BUTTON);
+  // Load the texture objects
+  // cv::Mat floorImage = cv::imread("C:\\OpenCVTexture\\floor.jpg"); // 利用openCV讀取圖片檔案
+  // if (floorImage.empty())
+  // {
+  //   std::cout << "Floor empty\n";
+  // }
+  // else
+  // {
+  //   // 將讀取進來的圖片檔案當作材質存進textures中
+  //   cv::flip(floorImage, floorImage, 0);
+  //   glGenTextures(1, &textures[0]);
+  //   glBindTexture(GL_TEXTURE_2D, textures[0]);
 
-  // RenderScene();
-  glutSpecialFunc(SpecialKeyHandler);
-  glutKeyboardFunc(NormalKeyHandler);
-  glutMouseFunc(MouseHandler);
+  //   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  //   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  //   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+  //   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 
-  glutReshapeFunc(ChangeSize);
-  glutDisplayFunc(RenderScene);
-  // glutIdleFunc(Loop);
-  glutTimerFunc(0, Timer, 0);
+  //   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, floorImage.cols, floorImage.rows, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, floorImage.ptr());
+  // }
 
-  glutMainLoop(); // http://www.programmer-club.com.tw/ShowSameTitleN/opengl/2288.html
-  return 0;
+  load_image(&textures[0], "./floor.jpg");
+  load_image(&textures[1], "./Block4.jpg");
+  load_image(&textures[2], "./Block5.jpg");
+  load_image(&textures[3], "./Block6.jpg");
+
+  // ...
 }
+
 void ChangeSize(int w, int h)
 {
-  printf("Window Size= %d X %d\n", w, h);
+  // Calculate new clipping volume
+  GLfloat windowWidth = 100.f;
+  GLfloat windowHeight = 100.f;
 
-  glEnable(GL_DEPTH_TEST);
-  glShadeModel(shadeType);
+  // Set the viewport to be the entire window
   glViewport(0, 0, w, h);
-  // windowx = w;
-  // windowy = h;
+
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  glOrtho(1.0 + padding, -1.0 - padding,
-          -1.0 - padding, 1.0 + padding,
-          0.01, 10000);
-}
-void RenderScene(void)
-{
 
-  glClearColor(0, 0, 0, 1.0);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  // Set the clipping volume
+  glOrtho(-100.0f, windowWidth, -100.0f, windowHeight, -200.0f, 200.0f);
 
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  gluLookAt(
-      camera.x, camera.y, camera.z,
-      target.x, target.y, target.z,
-      0, 1, 0);
 
-  glColor3f(1.0, 1.0, 1.0);
-  // x axis line
-  for (int i = 0; i <= lines; i++)
-  {
-    glBegin(GL_LINES);
-    float x = 2 * static_cast<float>(i) / lines - 1.0;
+  glLightfv(GL_LIGHT0, GL_POSITION, vLightPos);
 
-    glVertex3f(x, -1, 0);
-    glVertex3f(x, 1, 0);
-    glEnd();
-  }
-  // y axis line
-  for (int i = 0; i <= lines; i++)
-  {
-    glBegin(GL_LINES);
-    float y = 2 * static_cast<float>(i) / lines - 1.0;
-    glVertex3f(-1, y, 0);
-    glVertex3f(1, y, 0);
-    glEnd();
-  }
-  float unlines = 1.0 / lines;
-
-  // red points
-  for (int i = points.size() - 1; i >= 0; i--)
-  {
-    float pointx = remap(points[i].x, padding, lines);
-    float pointy = remap(points[i].y, padding, lines);
-    glColor3f(1.0, 0.0, 0.0);
-    glPointSize(windowx / (1.0 + padding) * unlines);
-    glBegin(GL_POINTS);
-    glVertex3f(pointx, pointy, 1);
-    glEnd();
-  }
-
-  auto loop = points;
-  auto loop_color = colors;
-  loop.push_back(points[0]);
-  loop_color.push_back(colors[0]);
-
-  for (int i = 0; i < loop.size() - 1; i++)
-  {
-    Vector2d p1 = loop[i];
-    Vector2d p2 = loop[i + 1];
-
-    Vector3d c1 = loop_color[i];
-    Vector3d c2 = loop_color[i + 1];
-
-    drawLine(p1, p2, c1, c2);
-  }
-  int id = 0;
-  // green lines
-
-  for (int k = 0; k < lines; k++)
-  {
-    for (int j = 0; j < lines; j++)
-    {
-      float px = blockIdCoord(floor(j));
-      float py = blockIdCoord(floor(k));
-      Vector2d p = {px, py};
-      bool inside = true;
-      Vector3d l_color = {0, 0, 0};
-      Vector3d r_color = {0, 0, 0};
-      Vector2d lp = {2, 2};
-      Vector2d rp = {2, 2};
-      for (int i = 0; i < loop.size() - 1; i++)
-      {
-        Vector2d p1 = loop[i];
-        Vector2d p2 = loop[i + 1];
-
-        Vector3d c1 = loop_color[i];
-        Vector3d c2 = loop_color[i + 1];
-        if (!isLeft(p1, p2, p))
-        {
-          inside = false;
-          break;
-        }
-
-        if (p1.y > p2.y)
-        {
-          Vector2d temp = p1;
-          p1 = p2;
-          p2 = temp;
-
-          Vector3d temp_color = c1;
-          c1 = c2;
-          c2 = temp_color;
-        }
-        if (p.y <= p1.y || p.y >= p2.y)
-          continue;
-
-        // drawLine(p1, p2, c1, c2);
-
-        auto edge_point = lerp(p1, p2, (p.y - p1.y) / (p2.y - p1.y));
-        edge_point = {blockId(edge_point.x, padding, lines), blockId(edge_point.y, padding, lines)};
-        edge_point = {blockIdCoord(edge_point.x + 1), blockIdCoord(edge_point.y)};
-        if (edge_point.x < p.x)
-        {
-          lp = edge_point;
-          l_color = lerp(c1, c2, (p.y - p1.y) / (p2.y - p1.y));
-        }
-        else
-        {
-          rp = edge_point;
-          rp.x -= 2.0 / lines;
-          r_color = lerp(c1, c2, (p.y - p1.y) / (p2.y - p1.y));
-        }
-      }
-      if (inside)
-      {
-        id += 1;
-        if (id > draw_id)
-        {
-          glColor3f(1, 1, 1);
-          glBegin(GL_POINTS);
-          glVertex3f(rp.x, rp.y, 2);
-          glEnd();
-
-          glBegin(GL_POINTS);
-          glVertex3f(lp.x, lp.y, 2);
-          glEnd();
-          break;
-        }
-
-        auto color = lerp(l_color, r_color, (p.x - lp.x) / (rp.x - lp.x));
-        glColor3f(color.x, color.y, color.z);
-
-        glBegin(GL_POINTS);
-        glVertex3f(px, py, 10);
-        glEnd();
-      }
-      if (id > draw_id)
-        break;
-    }
-  }
-  glutSwapBuffers();
+  glRotatef(30.0f, 1.0f, 0.0f, 0.0f);
+  glRotatef(330.0f, 0.0f, 1.0f, 0.0f);
 }
-// sleep
+
+int main(int argc, char *argv[])
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+  glutInitWindowSize(800, 600);
+  glutCreateWindow("Textures");
+  SetupRC();
+  glutReshapeFunc(ChangeSize);
+  glutDisplayFunc(RenderScene);
+  glutKeyboardFunc(MyKeyboard);
+
+  glutMainLoop();
+  glDeleteTextures(4, textures);
+  return 0;
+}
